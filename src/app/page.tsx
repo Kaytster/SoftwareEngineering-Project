66d4660d227cf14b472a03c56a5208704da439cf
Kaytster <ktsharps@gmail.com>
2025-10-28 14:18:23 +0000

    Added Icons to navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
-import Image from "next/image";
 import './globals.css'
+import Image from 'next/image';
+import homeIcon from './components/icons/home.png';
+import signupIcon from './components/icons/add-user.png';
+import loginIcon from './components/icons/avatar.png';
 
+
+// <a href="https://www.flaticon.com/free-icons/home-button" title="home button icons">Home button icons created by Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/free-icons/add-user" title="add user icons">Add user icons created by Freepik - Flaticon</a>
+// <a href="https://www.flaticon.com/free-icons/profile" title="profile icons">Profile icons created by Freepik - Flaticon</a>
 export default function Home() {
   return (
   <main>
@@ -8,13 +15,40 @@ export default function Home() {
     <header> 
         <ul className="flex justify-around bg-[#9CB7C8]">
           <li className="mr-3">
-            <a className="inline-block rounded-full py-2 px-4 bg-[#729458] hover:bg-[#3E592A] text-[#0C0C0C]" href="#">Home</a>
+            <a className="inline-block rounded-full py-2 px-4 bg-[#729458] hover:bg-[#3E592A] text-[#0C0C0C] flex items-center" href="#">
+              <Image 
+                  src={homeIcon} 
+                  alt="Home" 
+                  width={20} 
+                  height={20} 
+                  className="mr-1 inline-block" 
+              />
+              Home
+              </a>
           </li>
           <li className="mr-3">
-            <a className="inline-block rounded-full text-[#0C0C0C] hover:bg-[#B6D99B] py-2 px-4" href="#">Sign Up</a>
+            <a className="inline-block rounded-full text-[#0C0C0C] hover:bg-[#B6D99B] py-2 px-4 flex items-center" href="#">
+              <Image 
+                  src={signupIcon} 
+                  alt="Sign Up" 
+                  width={20} 
+                  height={20} 
+                  className="mr-1 inline-block" 
+              />
+              Sign Up
+            </a>
           </li>
           <li className="mr-3">
-            <a className="inline-block rounded-full text-[#0C0C0C] hover:bg-[#B6D99B] py-2 px-4" href="#">Log In</a>
+            <a className="inline-block rounded-full text-[#0C0C0C] hover:bg-[#B6D99B] py-2 px-4 flex items-center" href="#">
+              <Image 
+                  src={loginIcon} 
+                  alt="Log In" 
+                  width={20} 
+                  height={20} 
+                  className="mr-1 inline-block" 
+              />
+              Log In
+            </a>
           </li>
       </ul>
     </header>
