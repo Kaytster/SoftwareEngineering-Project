f3ce2302ec8ca4ce9af0c9d0814a3797c12ec811
Kaytster <ktsharps@gmail.com>
2025-11-10 10:19:52 +0000

    removed unneccesary pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,13 @@
 import './globals.css'
 import Image from 'next/image';
-import homeIcon from './components/icons/home.png';
-import signupIcon from './components/icons/add-user.png';
-import loginIcon from './components/icons/avatar.png';
+//import LandingNav from './components/landingNavigation';
 
-
-// <a href="https://www.flaticon.com/free-icons/home-button" title="home button icons">Home button icons created by Freepik - Flaticon</a>
-// <a href="https://www.flaticon.com/free-icons/add-user" title="add user icons">Add user icons created by Freepik - Flaticon</a>
-// <a href="https://www.flaticon.com/free-icons/profile" title="profile icons">Profile icons created by Freepik - Flaticon</a>
 export default function Home() {
   return (
   <main>
     {/* NAV BAR */}
     <header> 
-        <ul className="flex justify-around bg-[#9CB7C8]">
-          <li className="mr-3">
-            <a className="inline-block rounded-full py-2 px-4 bg-[#729458] hover:bg-[#3E592A] text-[#0C0C0C] flex items-center" href="#">
-              <Image 
-                  src={homeIcon} 
-                  alt="Home" 
-                  width={20} 
-                  height={20} 
-                  className="mr-1 inline-block" 
-              />
-              Home
-              </a>
-          </li>
-          <li className="mr-3">
-            <a className="inline-block rounded-full text-[#0C0C0C] hover:bg-[#B6D99B] py-2 px-4 flex items-center" href="#">
-              <Image 
-                  src={signupIcon} 
-                  alt="Sign Up" 
-                  width={20} 
-                  height={20} 
-                  className="mr-1 inline-block" 
-              />
-              Sign Up
-            </a>
-          </li>
-          <li className="mr-3">
-            <a className="inline-block rounded-full text-[#0C0C0C] hover:bg-[#B6D99B] py-2 px-4 flex items-center" href="#">
-              <Image 
-                  src={loginIcon} 
-                  alt="Log In" 
-                  width={20} 
-                  height={20} 
-                  className="mr-1 inline-block" 
-              />
-              Log In
-            </a>
-          </li>
-      </ul>
+        {/* <LandingNav /> */}
     </header>
 
       <div className="relative w-300 h-100 mx-auto mt-20">
