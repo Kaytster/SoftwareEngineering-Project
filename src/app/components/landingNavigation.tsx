648c40f2ac418c09aeebcb374b425dad0e1ddaff
Kaytster <ktsharps@gmail.com>
2025-11-26 16:32:12 +0000

    Added functionality to the Landing nav

--- src/app/components/landingNavigation.tsx
+++ src/app/components/landingNavigation.tsx
@@ -1,50 +1,44 @@
 // <a href="https://www.flaticon.com/free-icons/home-button" title="home button icons">Home button icons created by Freepik - Flaticon</a>
 // <a href="https://www.flaticon.com/free-icons/add-user" title="add user icons">Add user icons created by Freepik - Flaticon</a>
 // <a href="https://www.flaticon.com/free-icons/profile" title="profile icons">Profile icons created by Freepik - Flaticon</a>
-
+"use client"
 import Image from "next/image"
+import { usePathname } from "next/navigation";
 import homeIcon from './icons/home.png';
 import signupIcon from './icons/add-user.png';
 import loginIcon from './icons/avatar.png';
 export default function LandingNav() {
+  const currentPath = usePathname();
+  const links = [
+    {href: '/', label: 'Home', icon: homeIcon, path: '/'},
+    {href: '/signup', label: 'Sign Up', icon: signupIcon, path: '/signup'},
+    {href: '/login', label: 'Log In', icon: loginIcon, path: '/login'}
+  ];
+  const navItems = "inline-block rounded-full py-2 px-4 hover:bg-[#3E592A] text-[#0C0C0C] flex items-center";
+  const getLinks = (path: string) => {
+    const isActive = currentPath == path;
+    let linkItems = `${navItems} hover:bg-[#3b512a]`;
+    if (isActive) {
+      linkItems = `${navItems} bg-[#729458] hover:bg-[#3E592A]`;
+    }
+    return linkItems;
+  };
   return (
-        <ul className="flex justify-around bg-[#9CB7C8]">
-          <li className="mr-3">
-            <a className="inline-block rounded-full py-2 px-4 bg-[#729458] hover:bg-[#3E592A] text-[#0C0C0C] flex items-center" href="#">
-              <Image 
-                  src={homeIcon} 
-                  alt="Home" 
+    <ul className="flex justify-around bg-[#9CB7C8]">
+      {links.map((link) => (
+        <li key={link.path} className="mr-3">
+          <a className={getLinks(link.path)} href={link.href}>
+            <Image 
+                  src={link.icon} 
+                  alt={link.label} 
                   width={20} 
                   height={20} 
                   className="mr-1 inline-block" 
               />
-              Home
-              </a>
-          </li>
-          <li className="mr-3">
-            <a className="inline-block rounded-full text-[#0C0C0C] hover:bg-[#B6D99B] py-2 px-4 flex items-center" href="#">
-              <Image 
-                  src={signupIcon} 
-                  alt="Sign Up" 
-                  width={20} 
-                  height={20} 
-                  className="mr-1 inline-block" 
-              />
-              Sign Up
-            </a>
-          </li>
-          <li className="mr-3">
-            <a className="inline-block rounded-full text-[#0C0C0C] hover:bg-[#B6D99B] py-2 px-4 flex items-center" href="#">
-              <Image 
-                  src={loginIcon} 
-                  alt="Log In" 
-                  width={20} 
-                  height={20} 
-                  className="mr-1 inline-block" 
-              />
-              Log In
-            </a>
-          </li>
-      </ul>
+              {link.label}
+          </a>
+        </li>
+      ))}
+    </ul>
   )
 }
