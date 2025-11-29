25a9e200b5c8c0192c373b5a5b6771f7009a3c13
Kaytster <ktsharps@gmail.com>
2025-11-29 12:16:54 +0000

    Created the Alert Component

---
+++ src/app/components/alert.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import CloseIcon from "./closeIcon";
+
+type AlertType = "Success" | "Error" | "Warning";
+
+type AlertProps = {
+    type: AlertType;
+    message: string;
+    onClose: () => void;
+};
+
+const backgroundColours = {
+    Success: "#22C55E",
+    Error: "#EF4444",
+    Warning: "#F97316",
+};
+
+const Alert: React.FC<AlertProps> = ({type, message, onClose}) => {
+    return (
+        <div className="alert-box flex w-full rounded-lg flex-col max-w-[400px] transform items-start justidy-start gap-4 bg-white transition delay-200 duration 500 ease-in-out"
+             style={{zIndex: "100"}}>
+            <div className="flex w-full items-start justify-between p-3">
+                <span className="font-Inter text-[13px] font-medium leading-tight text-black">
+                    {message}
+                </span>
+                <span className="cursor-pointer duration-300 hover:opacity-70"
+                      onClick={onClose}>
+                    <CloseIcon />
+                </span>
+            </div>
+            <div className="flex h-4 w-full items-center justify-center rounded-b-lg"
+                 style={{backgroundColor: backgroundColours[type],}}>
+            </div>
+        </div>
+    );
+};
+
+export default Alert;
