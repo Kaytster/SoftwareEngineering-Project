8cc9412c2939a80fe45c897a060f2f7d70c8d897
Kaytster <ktsharps@gmail.com>
2025-12-02 11:36:00 +0000

    created getDb function in db.ts to get or create the connection

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,3 +1,12 @@
 import {open, Database} from 'sqlite';
 import sqlite3 from 'sqlite3';
 import path from 'path';
+
+let db: Database | null = null;
+
+//Get or create the database connection
+export async function getDb(): Promise<Database> {
+    if (db) {
+        return db;
+    }
+}
