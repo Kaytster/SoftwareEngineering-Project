ad51111eac881b0bb4363bafb6c824edcc339e49
Kaytster <ktsharps@gmail.com>
2025-12-02 10:35:09 +0000

    added a form to modify the alert content

--- src/app/systemSettings/notifications/page.tsx
+++ src/app/systemSettings/notifications/page.tsx
@@ -37,7 +37,7 @@ export default function Notifications() {
         updateMessage(selectedType, currentMessage);
         alert(`New message for ${selectedType} saved!`);
     };
-    
+
   return (
       <main>
           <header>
@@ -45,7 +45,40 @@ export default function Notifications() {
           </header>
           <div className="relative w-300 h-130 mx-auto mt-20">
               <div className="absolute inset-0 p-8 bg-[#E9F1F6] border-2 border-[#729458] rounded-lg shadow-xl z-20">
-                  <div className="flex space-x-6 h-full"></div>
+                  <div className="flex space-x-6 h-full">
+                    <h2 className='text-2xl font-bold mb-6 text-foreground'>
+                        Customize Alerts
+                    </h2>
+                    <div className='flex space-x-4 mb-8'>
+                        {['Success', 'Error', 'Warning'].map((type) => (
+                            <button className={getTypeStyles(type as AlertType, selectedType)}
+                                    key={type}
+                                    onClick={() => setSelectedType(type as AlertType)}
+                            >
+                                {type} Alert
+                            </button>
+                        ))}
+                    </div>
+
+                    <div className='flex flex-col'>
+                        <label htmlFor='alert-msg'
+                               className='mb-2 font-medium text-gray-700'>
+                                Alert Message Text: 
+                        </label>
+                        <input className='p-3 border border-gray-300 rounded-md text-black focus:ring-secondary focus:border-secondary'
+                               id='alert-msg'
+                               type='text'
+                               value={currentMessage}
+                               onChange={handleInputChange}
+                               placeholder='Enter message...'
+                        />
+                    </div>
+                    <button className='mt-6 px-6 py-2 bg-secondary text-white rounded-md font-bold hover:opacity-90 transition'
+                            onClick={handleSave}
+                    >
+                        Save New {selectedType} Message
+                    </button>
+                  </div>
                   {/* <button className='px-4 py-2 bg-green-500 text-white rounded'
                           onClick={() => showAlert('Success', 'success')}
                   >
@@ -61,10 +94,14 @@ export default function Notifications() {
                   >
                     Show Warning Alert
                   </button> */}
-
-
               </div>  
           </div>
+
+          <button className='px-4 py-2 bg-orange-500 text-white rounded'
+                          onClick={() => showAlert(selectedType, currentMessage)}
+          >
+                    Show Success Alert
+            </button>
       </main>
   )
 }
