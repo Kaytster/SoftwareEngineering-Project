02a459c99f5b7a33b4b239acac2558a7c4cf9d3e
wildflower2 <e.wythe@outlook.com>
2025-12-03 15:49:07 +0000

    donation-history nonfunctional

--- src/app/donation-history/page.tsx
+++ src/app/donation-history/page.tsx
@@ -3,6 +3,7 @@ import DonorNav from '../components/donorNavigation';
 import Image from 'next/image';
 import X from '../components/icons/X-image.png';
 import sampleImg from '../components/icons/sampleIMG.jpg';
+import ItemListed from '../components/item-listed';
 export default function DonationHistory() {
     return (
     <main>
@@ -12,10 +13,10 @@ export default function DonationHistory() {
         <body>
             <div className="flex flex-col p-10 justify-center">
                 {/* labels sorted by */}
-                <div className="flex flex-row justify-left">
-                    <button className=" mb-4 bg-[#729458] hover:bg-[#B6D99B] text-[#0C0C0C] font-bold py-2 px-4 rounded-full">
+                <div className="flex flex-row justify-left gap-3">
+                    <button className="mb-4 bg-[#729458] hover:bg-[#B6D99B] text-[#0C0C0C] font-bold py-2 px-4 rounded-full">
                         Today
-                        <Image src={X} alt="X" width={20} height={20} className="mr-1 inline-block w-4  h-4 rounded-full"/>
+                        <Image src={X} alt="X" width={20} height={20} className=" mr-1 inline-block w-4  h-4 rounded-full"/>
                     </button>
                     <button className=" mb-4 bg-[#729458] hover:bg-[#B6D99B] text-[#0C0C0C] font-bold py-2 px-4 rounded-full">
                         Coat
@@ -24,48 +25,11 @@ export default function DonationHistory() {
                 </div>
                 {/* items list */}
                 <div className="flex flex-col p-10 justify-center">
-                    {/* 1 */}
-                    <div className="flex flex-row justify-center">
-                        <Image src={sampleImg} alt="item" width={20} height={20} className="mr-1 inline-block w-20  h-20 rounded-full"/>
-                        <div className=" ">
-                            <div className="relative w-250 h-20 mx-auto mb-1 bg-[#9CB7C8] rounded-md flex flex-col items-center justify-center"> 
-                                Name of item + details
-                            </div>
-                        </div>
-                        <button className="ml-4 h-10 bg-[#729458] hover:bg-[#B6D99B] text-[#0C0C0C] font-bold py-2 px-4 rounded-full">
-                            Accepted
-                        </button>
-                    </div>
-                    {/* 2 */}
-                    <div className="flex flex-row justify-center">
-                        <Image src={sampleImg} alt="item" width={20} height={20} className="mr-1 inline-block w-20  h-20 rounded-full"/>
-                        <div className=" ">
-                            <div className="relative w-250 h-20 mx-auto mb-1 bg-[#9CB7C8] rounded-md flex flex-col items-center justify-center"> </div>
-                        </div>
-                        <button className="ml-4 h-10 bg-[#729458] hover:bg-[#B6D99B] text-[#0C0C0C] font-bold py-2 px-4 rounded-full">
-                            Accepted
-                        </button>
-                    </div>
-                    {/* 3 */}
-                    <div className="flex flex-row justify-center">
-                        <Image src={sampleImg} alt="item" width={20} height={20} className="mr-1 inline-block w-20  h-20 rounded-full"/>
-                        <div className=" ">
-                            <div className="relative w-250 h-20 mx-auto mb-1 bg-[#9CB7C8] rounded-md flex flex-col items-center justify-center"> </div>
-                        </div>
-                        <button className="ml-4 h-10 bg-[#729458] hover:bg-[#B6D99B] text-[#0C0C0C] font-bold py-2 px-4 rounded-full">
-                            Accepted
-                        </button>
-                    </div>
-                    {/* 4 */}
-                    <div className="flex flex-row justify-center">
-                        <Image src={sampleImg} alt="item" width={20} height={20} className="mr-1 inline-block w-20  h-20 rounded-full"/>
-                        <div className=" ">
-                            <div className="relative w-250 h-20 mx-auto mb-1 bg-[#9CB7C8] rounded-md flex flex-col items-center justify-center"> </div>
-                        </div>
-                        <button className="ml-4 h-10 bg-[#729458] hover:bg-[#B6D99B] text-[#0C0C0C] font-bold py-2 px-4 rounded-full">
-                            Accepted
-                        </button>
-                    </div>
+                    <ItemListed />
+                    <ItemListed />
+                    <ItemListed />
+                    <ItemListed />
+                    <ItemListed />
                 </div>
             </div>
         </body>
