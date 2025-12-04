b80bd226bff6dae207fea2077f1f419bd3f623e8
c4019544 <zinthanwarat89@gmail.com>
2025-12-04 16:44:35 +0000

    u

--- src/app/view-inventory/page.tsx
+++ src/app/view-inventory/page.tsx
@@ -68,7 +68,7 @@ export default function DisplayInventory() {
                         <td className="p-3 border">{d.Status}</td>
                         <td className="p-3 border">
                             <button className="bg-[#729458] text-[18px] text-white px-4 py-2 rounded-full hover:bg-[#B6D99B] cursor-pointer">
-                                Edit status
+                                Edit
                             </button>
                         </td>
                     </tr>
