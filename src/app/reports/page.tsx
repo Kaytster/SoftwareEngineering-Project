4182a1539b544b6b38ddad3aea1472bfa5bc1373
Kaytster <ktsharps@gmail.com>
2025-12-10 18:01:03 +0000

    Added a basic test linechart

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -2,15 +2,44 @@
 import '@/app/globals.css'
 import Image from 'next/image';
 import AdminNav from '@/app/components/adminNavigation';
-import { useEffect, useRef } from 'react';
+import { Line, LineChart } from 'recharts';
+
+//How many active users
+//How many accepted donations
+//How many donations created
 
 export default function Reports() {
 
+    const data = [
+        {
+            name: 'Item 1',
+            uv: 400,
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: 'Item 2',
+            uv: 300,
+            pv: 4567,
+            amt: 2400,
+        },
+        {
+            name: 'Item 3',
+            uv: 320,
+            pv: 2398,
+            amt: 2400,
+        },
+        {
+            name: 'Item 4',
+            uv: 200,
+            pv: 9800,
+            amt: 2400,
+        },
+    ];
 
   return (
-    <main>
+      <main>
         
-
         <header>
             <AdminNav />
         </header>
@@ -18,7 +47,10 @@ export default function Reports() {
         <div className="relative w-300 h-100 mx-auto mt-20">
             <div className="absolute inset-0 p-8 bg-[#E9F1F6] border-2 border-[#729458] rounded-lg shadow-xl z-20">
                 <div className="flex space-x-6 h-full">
-                    
+                    <LineChart style={{width: '100%', aspectRatio: 1.618, maxWidth: 600}}
+                               responsive data={data}>
+                        <Line dataKey="uv" />
+                    </LineChart>
                 </div>
             </div>  
         </div>
