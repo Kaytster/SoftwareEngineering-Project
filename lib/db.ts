6d4707198a927d88d33d393e7e244b7dc5b67339
Kaytster <ktsharps@gmail.com>
2025-12-11 14:00:40 +0000

    Hopefully removed CreatedAt col from database

--- lib/db.ts
+++ lib/db.ts
@@ -25,5 +25,35 @@ export async function getDb(): Promise<Database> {
         Email TEXT UNIQUE NOT NULL,
         PasswordHash TEXT NOT NULL);
         `);
+
+    //Adding data to AcceptedDate
+    // const donationCountResult = await db.get<{count: number}>(
+    //     `SELECT COUNT(*) AS count FROM Donations`
+    // );
+    // if (donationCountResult && donationCountResult.count === 0) {
+    //     console.log("Database Empty");
+
+    //     await db.exec(`INSERT
+    //         INTO DONATION (DonationID, ItemID, ImageID, DonorID, CharityID
+    //         )
+    //         `)
+    // }
+
+    // try {
+       
+    //     const columns = await db.all(`PRAGMA table_info(Donation)`);
+    //     const createdAtIndex = columns.findIndex(col => col.name === 'CreatedAt');
+    //     if (createdAtIndex !== -1) {
+    //         console.log("Found 'CreatedAt' column. Attempting to drop...");
+    //     await db.exec(`
+    //         ALTER TABLE Donation 
+    //         DROP COLUMN CreatedAt;
+    //     `);
+    //     console.log("Successfully dropped 'CreatedAt' column.");
+    //     }
+    // } catch (error) {
+    //     console.warn("Error during CreatedAt column migration. If column still exists, use DB Browser.", error);
+    // }
+
     return db;
 }
